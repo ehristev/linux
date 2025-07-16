f4bbf50112f92c28eaee2d3554cfb501315ccf57
Eugen Hristev <eugen.hristev@linaro.org>
2025-07-16 13:33:15 +0300

    sched/core: Annotate static information into Kmemdump
    Annotate vital static information into kmemdump:
     - runqueues
    
    Information on these variables is stored into dedicated kmemdump section.
    
    Signed-off-by: Eugen Hristev <eugen.hristev@linaro.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -66,6 +66,7 @@
 #include <linux/vtime.h>
 #include <linux/wait_api.h>
 #include <linux/workqueue_api.h>
+#include <linux/kmemdump.h>
 
 #ifdef CONFIG_PREEMPT_DYNAMIC
 # ifdef CONFIG_GENERIC_ENTRY
@@ -117,6 +118,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(sched_update_nr_running_tp);
 EXPORT_TRACEPOINT_SYMBOL_GPL(sched_compute_energy_tp);
 
 DEFINE_PER_CPU_SHARED_ALIGNED(struct rq, runqueues);
+KMEMDUMP_VAR_CORE(runqueues, sizeof(runqueues));
 
 #ifdef CONFIG_SCHED_DEBUG
 /*
